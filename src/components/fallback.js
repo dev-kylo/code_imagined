c431d5b1448c8d4ff63d1b967981cb41d2b094ce
Kyle Robinson <kylerobbo12@gmail.com>
2021-04-17 14:35:27 +0200

    gatsby lazy load fallback

--- src/components/fallback.js
+++ src/components/fallback.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import styled from 'styled-components';
-import fallback from '../images/outline_eci.png'
+import { graphql, useStaticQuery } from "gatsby";
+import Img from 'gatsby-image';
+
 
 
 const Container = styled.div`
@@ -16,11 +18,22 @@ const Container = styled.div`
 
 
 const Fallback = props => {
-
+    const eci = useStaticQuery(graphql`
+    query ecifallback{
+        file(relativePath: { eq: "outline_eci.png" }) {
+        childImageSharp {
+            # Specify the image processing specifications right in the query.
+            fluid {
+            ...GatsbyImageSharpFluid
+            }
+        }
+        }
+    }
+    `);
 
     return (
         <Container>
-            <img src={fallback} alt="Stencil outline of the Execution Context illustration" />
+            <Img fluid={eci.file.childImageSharp.fluid} alt="Stencil outline of the Execution Context illustration" />
         </Container>
     )
 }
