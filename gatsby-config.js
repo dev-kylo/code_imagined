906d277db82315c1005d452e7c521b61688fc735
Kyle Robinson <kylerobbo12@gmail.com>
2021-12-24 19:29:01 +0400

    use mailchimp plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -73,6 +73,13 @@ module.exports = {
           include: /images/ // See below to configure properly
         }
       }
+    },
+    {
+      resolve: 'gatsby-plugin-mailchimp',
+      options: {
+          endpoint: 'https://codeimagined.us7.list-manage.com/subscribe/post?u=4d0492b044ab95eee11a945a2&amp;id=5649b5fa7d', // string; add your MC list endpoint here; see instructions below
+          timeout: 3500, // number; the amount of time, in milliseconds, that you want to allow mailchimp to respond to your request before timing out. defaults to 3500
+      },
     }
   ],
 }
