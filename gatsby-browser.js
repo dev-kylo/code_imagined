ad382f2688f088ab6cce52ea74f9b27e789d0e82
Kyle <kyle@hitpixel.com>
2023-08-17 15:54:59 +0400

    install mantine

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,14 +1,18 @@
+/* eslint-disable import/no-extraneous-dependencies */
 /* eslint-disable react/no-unstable-nested-components */
 // Example gatsby-browser.js and gatsby-ssr.js file
 import './src/stylesheets/nightowl_prism_theme.css'
 import * as React from 'react'
 import { Link } from 'gatsby'
 import { PrismicProvider } from '@prismicio/react'
+import { MantineProvider } from '@mantine/core'
 
 function App({ children }) {
     return (
         <PrismicProvider internalLinkComponent={({ href, ...props }) => <Link to={href} {...props} />}>
-            {children}
+            <MantineProvider withGlobalStyles withNormalizeCSS>
+                {children}
+            </MantineProvider>
         </PrismicProvider>
     )
 }
--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,14 +1,29 @@
+/* eslint-disable import/no-extraneous-dependencies */
 /* eslint-disable react/no-unstable-nested-components */
 // Example gatsby-browser.js and gatsby-ssr.js file
 
 import * as React from 'react'
 import { Link } from 'gatsby'
 import { PrismicProvider } from '@prismicio/react'
+import { createStylesServer, ServerStyles } from '@mantine/ssr'
+import { renderToString } from 'react-dom/server'
+import { MantineProvider } from '@mantine/core'
 
 function App({ children }) {
     return (
         <PrismicProvider internalLinkComponent={({ href, ...props }) => <Link to={href} {...props} />}>
-            {children}
+            <MantineProvider withGlobalStyles withNormalizeCSS>
+                {children}
+            </MantineProvider>
         </PrismicProvider>
     )
+}
+
+// optional: you can provide your cache as a first argument in createStylesServer function
+const stylesServer = createStylesServer()
+
+export const replaceRenderer = ({ bodyComponent, replaceBodyHTMLString, setHeadComponents }) => {
+    const html = renderToString(bodyComponent)
+    setHeadComponents([<ServerStyles html={html} server={stylesServer} />])
+    replaceBodyHTMLString(html)
 }
