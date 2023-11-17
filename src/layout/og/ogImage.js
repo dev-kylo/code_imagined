df00787d1244064e6ba381126d674f0a36f02ce8
Kyle <kyle@hitpixel.com>
2023-11-17 13:41:12 +0400

    update og path

--- src/layout/og/ogImage.js
+++ src/layout/og/ogImage.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import { StaticImage } from 'gatsby-plugin-image'
 
-const ogImage = () => <StaticImage src="../images/magePreview.png" alt="A beam of yellow light" placeholder="blurred" />
+const ogImage = () => (
+    <StaticImage src="../../images/magePreview.png" alt="A beam of yellow light" placeholder="blurred" />
+)
 
 export default ogImage
