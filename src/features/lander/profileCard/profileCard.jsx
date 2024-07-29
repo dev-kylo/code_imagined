750494d79c8fdaa8b3988f5b859bbabc78e038c6
Kyle Robinson <kyle@hitpixel.com>
2024-07-29 08:48:13 +0200

    add logos

--- src/features/lander/profileCard/profileCard.jsx
+++ src/features/lander/profileCard/profileCard.jsx
@@ -59,9 +59,77 @@ const Canvas = styled.div`
     }
 `
 
+const Featured = styled.div`
+    height: 150px;
+    position: relative;
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    vertical-align: center;
+    img {
+        object-fit: cover;
+        max-width: 100%;
+        height: auto;
+        vertical-align: center;
+        padding: 0;
+        margin: 0;
+    }
+`
+
+const ProfileImageContainer = styled.div`
+    flex-grow: 1;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    background: ${({ theme }) => theme.colors.navy};
+    z-index: 100;
+    img {
+        width: 100%;
+        height: 100%;
+        object-fit: cover;
+    }
+`
+
+const ProfileLogos = styled.div`
+    display: flex;
+    flex-direction: column;
+    height: 100%;
+`
+
+const LogoContainer = styled.div`
+    height: 100%;
+    background: ${({ color }) => color || 'white'};
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    padding: 0 1rem;
+`
+
 const ProfileCard = () => (
     <Card>
-        <StaticImage src="../../../images/Kylo.jpg" alt="Profile picture of Kylo" placeholder="blurred" />
+        <ProfileLogos>
+            <ProfileImageContainer>
+                <StaticImage src="../../../images/Kylo.jpg" alt="Profile picture of Kylo" placeholder="blurred" />
+            </ProfileImageContainer>
+            <Featured>
+                <LogoContainer color="#9012fd">
+                    <StaticImage
+                        src="../../../images/codenewbie.png"
+                        alt="CodeNewbie logo"
+                        placeholder="blurred"
+                        objectFit="fill"
+                        style={{ background: '#9012fd' }}
+                    />
+                </LogoContainer>
+                <LogoContainer>
+                    <StaticImage
+                        src="../../../images/SmashingMag.jpg"
+                        alt="Smashing Magazine logo"
+                        placeholder="blurred"
+                        objectFit="fill"
+                    />
+                </LogoContainer>
+            </Featured>
+        </ProfileLogos>
         <Canvas>
             <H2 style={{ color: 'black' }}>Learn JavaScript the RIGHT way </H2>
             <P style={{ color: 'black' }}> Hi, I'm Kylo 👋.</P>
