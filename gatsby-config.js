ad5a7dbd5a96fd448c1cf29689ffaedfcaf3e638
devkylo <kylerobbo12@gmail.com>
2025-06-27 14:40:23 +0400

    more deploy changes v2

--- gatsby-config.js
+++ gatsby-config.js
@@ -17,7 +17,7 @@ module.exports = {
         `gatsby-transformer-sharp`,
         `gatsby-plugin-sharp`,
         `gatsby-plugin-styled-components`,
-        `gatsby-plugin-open-graph-images`,
+        // `gatsby-plugin-open-graph-images`, // Temporarily removed for debugging
         `gatsby-transformer-remark`,
         {
             resolve: 'gatsby-plugin-robots-txt',
